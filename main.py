077b56c94e56037f6ab8e750df7c819c1b78a263
molfaryura <oleksandrr1996@protonmail.com>
2023-06-04 12:42:35 +0300

    Created get_all_books(), issue #1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request, render_template
 
 from dotenv import load_dotenv
 
-from models import db
+from models import db, Books, Author
 
 app = Flask(__name__)
 
@@ -15,13 +15,34 @@ DB_PASSWORD = os.environ.get('DB_PWD')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{DB_PASSWORD}@localhost/postgres_database'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#db.init_app(app)
+db.init_app(app)
 #with app.app_context():
 #    db.create_all()
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/get/all_books')
+def get_all_books():
+    author_name = request.args.get('author')
+
+    if author_name is None:
+        books = db.session.query(Books).all()
+    else:
+        author = Author.query.filter_by(name=author_name).first()
+
+        if author:
+            books = Books.query.filter_by(author=author).all()
+        else:
+            return jsonify(error='This author does not exists'), 404
+
+
+    if books:
+        all_books = [{'id': book.id, 'title': book.title} for book in books]
+        return jsonify(books=all_books), 200
+    else:
+        return jsonify(error='Sorry, can not find any books'), 404
 
 
 if __name__ == '__main__':

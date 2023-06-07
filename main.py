b1f67558e0a4f0f8436000b208d5af2b658901e4
molfaryura <oleksandrr1996@protonmail.com>
2023-06-07 14:09:11 +0300

    Created admin functionality

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
 
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, url_for, flash
+
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 from dotenv import load_dotenv
 
 from models import db, Books, Author, Notes, Chapters
 
+from admin import Admin
+
 app = Flask(__name__)
 
 load_dotenv()
+
+app.secret_key = os.environ.get('SECRET_KEY')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 DB_PASSWORD = os.environ.get('DB_PWD')
 
@@ -16,9 +25,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{DB_PASSWORD}@lo
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Admin.query.get(user_id)
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form['password']
+
+        admin = Admin.query.filter_by(username=username).first()
+        if admin and admin.check_password(password=password):
+            login_user(admin)
+            return redirect(url_for('admin_dashboard'))
+        else:
+            flash('Invalid username or password')
+            return redirect(url_for('admin'))
+
+    return render_template('admin_login.html')
+
+@app.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    return 'Welcome to the admin dashboard!'
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/get/all_books')
 def get_all_books():
@@ -68,9 +108,6 @@ def get_notes():
             all_notes.append({'id': note.id, 'book': book.title, 'content': note.content, 'chapter': chapter.chapter_name})
 
         return jsonify(all_notes), 200
-
-    
-
 
 if __name__ == '__main__':
     db.init_app(app)

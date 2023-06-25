9387c5721266b8686f00e0346f584a63201850f0
molfaryura <oleksandrr1996@protonmail.com>
2023-06-25 11:49:45 +0300

    Created helper func, updated selenium tests

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -18,6 +18,37 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def fill_forms(browser, **kwargs):
+    """
+    Helper function to fill in form fields and submit the form in a Selenium browser instance.
+
+    Args:
+        browser (WebDriver): The Selenium WebDriver instance representing the browser.
+        **kwargs: Keyword arguments containing form field values. Expected keys are:
+            - 'username': Value for the username field.
+            - 'password': Value for the password field.
+            - 'secret_word': Value for the secret word field.
+
+    Returns:
+        None
+    """
+
+    username = browser.find_element(By.ID, 'typeUsernameX')
+    username.click()
+    username.send_keys(kwargs['username'])
+
+    password = browser.find_element(By.ID, 'typePasswordX')
+    password.click()
+    password.send_keys(kwargs['password'])
+
+    secret_word = browser.find_element(By.ID, 'typeSecretX')
+    secret_word.click()
+    secret_word.send_keys(kwargs['secret_word'])
+
+    submit_button = browser.find_element(By.TAG_NAME, 'button')
+    submit_button.submit()
+
+
 def test_get_admin_page(browser):
     """
     Test case to verify accessing the admin page.
@@ -34,27 +65,37 @@ def test_get_admin_page(browser):
     assert 'Login' in browser.title
 
 
-def test_login_admin_invalid(browser):
+def test_login_admin_invalid_secret_word(browser):
     """
-    Test case to verify login functionality with invalid credentials.
+    Test case to verify login functionality with invalid secret word.
+
+    The test enters an invalid secret word and invalid username and passowrd, and submits the form.
+    It asserts that the user is redirected to the youtube video.
+    """
+
+    browser.get('http://localhost:5000/admin')
+
+    fill_forms(browser, username='admin',
+               password='admin', secret_word='admin')
+
+    assert browser.current_url == 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+
+def test_login_admin_valid_secret_word(browser):
+    """
+    Test case to verify login functionality with valid secret word and
+    invalid username and password.
 
     The test enters an invalid username and password, submits the form,
     and waits for the error message to appear.
     It asserts that the displayed error message is 'Error : Invalid username or password'.
     """
 
+    secret = os.environ.get('TEST_SECRET')
+
     browser.get('http://localhost:5000/admin')
 
-    username = browser.find_element(By.ID, 'typeUsernameX')
-    username.click()
-    username.send_keys('admin')
-
-    password = browser.find_element(By.ID, 'typePasswordX')
-    password.click()
-    password.send_keys('admin')
-
-    submit_button = browser.find_element(By.TAG_NAME, 'button')
-    submit_button.submit()
+    fill_forms(browser, username='admin', password='admin', secret_word=secret)
 
     wait = WebDriverWait(browser, 10)
 
@@ -65,7 +106,7 @@ def test_login_admin_invalid(browser):
     assert error_message.text == 'Error : Invalid username or password'
 
 
-def test_login_admin_valid(browser):
+def test_login_admin_valid_all(browser):
     """
     Test case to verify login functionality with valid credentials.
     It enters the valid admin credentials, submits the form,
@@ -75,19 +116,12 @@ def test_login_admin_valid(browser):
 
     admin_username = os.environ.get('ADMIN_USERNAME')
     admin_password = os.environ.get('ADMIN_PASSWORD')
+    secret = os.environ.get('TEST_SECRET')
 
     browser.get('http://localhost:5000/admin')
 
-    username = browser.find_element(By.ID, 'typeUsernameX')
-    username.click()
-    username.send_keys(admin_username)
-
-    password = browser.find_element(By.ID, 'typePasswordX')
-    password.click()
-    password.send_keys(admin_password)
-
-    submit_button = browser.find_element(By.TAG_NAME, 'button')
-    submit_button.submit()
+    fill_forms(browser, username=admin_username,
+               password=admin_password, secret_word=secret)
 
     redirected_url = browser.current_url
     expected_url = 'http://localhost:5000/admin/interface'
